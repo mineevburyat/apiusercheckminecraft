44a0ccb5688eb65d51b22a59aae2715ee759ec7b
Mineev Buryat <mineev.buryat@gmail.com>
2025-08-20 21:38:15 +0800

    fix

--- TG_bot_alt/config.py
+++
@@ -1,6 +0,0 @@
-BOT_TOKEN = "1783887074:AAFE5g0upRn8p1spKydhP76JT6CPg5bHlUg"
-
-API_REG = "http://127.0.0.1:8000/api/add_nick"
-API_SAVE = "http://127.0.0.1:8000/api/save"
-API_DEL = "http://127.0.0.1:8000/api/del_nick"
-API_CHEAK = "server"
